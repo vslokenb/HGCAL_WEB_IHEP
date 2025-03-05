74d4d7ea8033848a34a979db73cdba36b7235422
Zirui Wang <wangzirui7@gmail.com>
2025-03-05 14:05:10 +0100

    Update website.py

--- IHEP_MAC_Bookkeeping/website.py
+++ IHEP_MAC_Bookkeeping/website.py
@@ -1550,7 +1550,7 @@ def main():
 
     if show_image:
         st.title("Login")
-        st.image("CMS_detector.jpeg", use_container_width=True)
+        st.image("IHEP_MAC_Bookkeeping/CMS_detector.jpeg", use_container_width=True)
         st.write("Welcome to the HGCal lab")
 
         if not login_button and not logged_in:
